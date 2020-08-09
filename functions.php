fda99828894749bc365b94992b55219265b4f35a
Michael Arawole <39175160+michaelarawole@users.noreply.github.com>
2020-08-09 16:33:01 +0100

    Update functions.php

--- functions.php
+++ functions.php
@@ -4,7 +4,7 @@
 // | @author_url	: https://logad.net
 // | @author_email	: henttech@gmail.com   
 // +------------------------------------------------------------------------+
-// | Date : 31st May 2020
+// | Last Updated : 31st May 2020
 // +------------------------------------------------------------------------+
 
 ## Useful Custom PHP functions ##
@@ -113,5 +113,26 @@ function generateOTP($n) {
         return $result; 
 }
 
+// Hide emails (http://www.maurits.vdschee.nl/php_hide_email/)
+function hide_email($email)
 
+{ $character_set = '+-.0123456789@ABCDEFGHIJKLMNOPQRSTUVWXYZ_abcdefghijklmnopqrstuvwxyz';
+
+  $key = str_shuffle($character_set); $cipher_text = ''; $id = 'e'.rand(1,999999999);
+
+  for ($i=0;$i<strlen($email);$i+=1) $cipher_text.= $key[strpos($character_set,$email[$i])];
+
+  $script = 'var a="'.$key.'";var b=a.split("").sort().join("");var c="'.$cipher_text.'";var d="";';
+
+  $script.= 'for(var e=0;e<c.length;e++)d+=b.charAt(a.indexOf(c.charAt(e)));';
+
+  $script.= 'document.getElementById("'.$id.'").innerHTML="<a href=\\"mailto:"+d+"\\">"+d+"</a>"';
+
+  $script = "eval(\"".str_replace(array("\\",'"'),array("\\\\",'\"'), $script)."\")"; 
+
+  $script = '<script type="text/javascript">/*<![CDATA[*/'.$script.'/*]]>*/</script>';
+
+  return '<span id="'.$id.'">[javascript protected email address]</span>'.$script;
+
+}
 //More to come :D
